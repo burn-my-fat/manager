66cbfde5a79e37666975d5a4d3219316445f9dbc
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-10-19 23:29:17 +0300

    Add Firebase support

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBnFJzF5r39-xLSDOtjs4Kb52TjKXzoKl8',
+      authDomain: 'manager-c3bc9.firebaseapp.com',
+      databaseURL: 'https://manager-c3bc9.firebaseio.com',
+      projectId: 'manager-c3bc9',
+      storageBucket: 'manager-c3bc9.appspot.com',
+      messagingSenderId: '733340301178'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
