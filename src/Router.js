b5a642bb86f340db0cf80289881d17a639f20523
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-10-22 23:59:16 +0300

    Navigate to the Employee Creation Form

--- src/Router.js
+++ src/Router.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { Stack, Scene, Router } from 'react-native-router-flux';
+import { Stack, Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Stack key="root" hideNavBar>
         <Scene key="auth">
-          <Scene key="login" component={LoginForm} title="Please Login" initial />
+          <Scene key="login" component={LoginForm} title="Please Login" />
         </Scene>
+
         <Scene key="main">
           <Scene
             rightTitle="Add"
-            onRight={() => { console.log('right'); }}
+            onRight={() => { Actions.employeeCreate(); }}
             key="employeeList"
             component={EmployeeList}
             title="Employees"
+            initial
           />
+
+          <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
         </Scene>
       </Stack>
     </Router>
