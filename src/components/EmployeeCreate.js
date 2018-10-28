c0cae320fcb2d009442aa56c768344a2b7eabd6d
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-10-28 23:20:27 +0300

    Clear Form Attributes

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeCreate } from '../actions';
+import { employeeUpdate, employeeCreate, employeeClear } from '../actions';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeCreate extends Component {
+  componentWillMount() {
+    this.props.employeeClear();
+  }
+
   onButtonPress() {
     const { name, phone, shift } = this.props;
 
@@ -33,5 +37,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, {
-  employeeUpdate, employeeCreate
+  employeeUpdate, employeeCreate, employeeClear
 })(EmployeeCreate);
